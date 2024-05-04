da715e58a6b27225fef964ac3d75d6dc7228c1df
anikmondol <anikmondol558363@gmil.com>
2024-05-04 21:33:48 +0600

    six ways to do conditional rendering: if, ternary, &&, ||

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,7 +5,7 @@
 // }
 
 
-// condition rendering option 1:
+// // condition rendering option 1:
 // export default function Todo({ task, isDone }) {
 //     if(isDone){
 //         return <li>Finished: {task}</li>
@@ -15,9 +15,46 @@
 // }
 
 
+// condition rendering option 2:
+// export default function Todo({task, isDone}){
+//     if(isDone){
+//         return <li>Finished: {task}</li>
+//     }
+//     return <li>Work on: {task}</li>
+// }
+
+
+// condition rendering option: 3, ternary operator
+// export default function Todo({task, isDone}){
+//     return (
+//         <li>{isDone ? "Finished" : "Work on"}: {task}</li>
+//     )
+// }
+
+
+// conditional rendering option 4: &&
+// export default function Todo({task, isDone}){
+//     return (
+//         <li>{task}: {isDone && ': Done'} </li>
+//     )
+// }
+
+
+// conditional rendering option 5: ||
+// export default function Todo({task, isDone}){
+//     return(
+//         <li>{task}: {isDone || ': work on'}</li>
+//     )
+// }
+
+
+// conditional rendering option 6
 export default function Todo({task, isDone}){
+    let item;
     if(isDone){
-        return <li>Finished: {task}</li>
+        item = <li>Task: {task}</li>
+    }else{
+        item = <li>work on: {task}</li>
     }
-    return <li>Work on: {task}</li>
+    return item;
 }
